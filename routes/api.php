270a0ebf214ed0ed0cb4f328615d0110e6cbb815
marxlima <marxmaciel@gmail.com>
2024-03-08 03:39:39 -0300

    first commit

---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return response()->json([
+        'success' => true,
+    ]);
+});
+
+
+
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
